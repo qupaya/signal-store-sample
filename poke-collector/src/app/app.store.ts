8e6a0fc85446afbdb3fdf302186d741041215055
patrickjahr <patrick@qupaya.com>
2024-04-26 13:37:09 +0200

    add load gen method

--- poke-collector/src/app/app.store.ts
+++ poke-collector/src/app/app.store.ts
@@ -1,5 +1,9 @@
 import {Generation} from "./models/generation";
-import {signalStore, withState} from "@ngrx/signals";
+import {patchState, signalStore, withMethods, withState} from "@ngrx/signals";
+import {inject} from "@angular/core";
+import {ApiService} from "./services/api.service";
+import {rxMethod} from "@ngrx/signals/rxjs-interop";
+import {map, pipe, switchMap, tap} from "rxjs";
 
 type PokemonCollectorState = {
   loading: boolean;
@@ -15,5 +19,15 @@ export const pokemonCollectorStore = signalStore(
   {
     providedIn: 'root'
   },
-  withState(initialState)
+  withState(initialState),
+  withMethods((store, apiService = inject(ApiService)) => ({
+    loadGenerations: rxMethod<void>(
+      pipe(
+        tap(() => patchState(store, {loading: true})),
+        switchMap(() => apiService.generations()),
+        map(({data}) => data.items),
+        tap(generations => patchState(store, {loading: false, generations})),
+      )
+    )
+  }))
 )
